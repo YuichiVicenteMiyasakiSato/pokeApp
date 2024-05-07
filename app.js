895ba8a0d0af3e8b9f0b120eb66c86913abd5ca6
yuichi <yuichivmiyasakisato1331.com>
2024-05-07 16:59:34 -0600

    Primer Version

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const namePoke2 = document.querySelector('#nombrePoke-rival');
 const poke2Tipo = document.querySelector('#tipoRival');
 const poke2Ataque = document.querySelector('#ataqueRival');
 
+const imgPoke = document.querySelector('#poke');
+const namePoke = document.querySelector('#nombrePoke-propio');
+const pokeTipo = document.querySelector('#tipoPropio');
+const pokeAtaque = document.querySelector('#ataquePropio');
+
+
+const input = document.querySelector('#input');
+const btnElegir = document.querySelector('#boton_poke');
+const btnPelear = document.querySelector('#combate');
 
 const getNumRandom = () => {
     let min = Math.ceil(0);
@@ -12,6 +21,20 @@ const getNumRandom = () => {
   }
 
 const obtenerPokePropio = ()=>{
+    const num = input.value;
+    
+    
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`).then((res)=>{ // axios llama a la api mediante una URL
+    
+            return res.data // devuelve la respuesta de los datos del API, then se espera a que responda el resultado
+        }).then((res)=>{ // ya teniendo la respuesta de la API, junto con sus datos
+            console.log(res);
+            imgPoke.src = res.sprites.back_default;
+            pokeTipo.innerHTML = res.types[0].type.name;
+            namePoke.innerHTML = res.name;
+            pokeAtaque.innerHTML = res.stats[0].base_stat;
+        })
+    
 
 }
 
@@ -19,10 +42,10 @@ const obtenerPokeRival = () =>{
 
     const numPokeRival = getNumRandom();
 
-    axios.get(`https://pokeapi.co/api/v2/pokemon/${numPokeRival}`).then((res)=>{
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${numPokeRival}`).then((res)=>{ // axios llama a la api mediante una URL
 
-        return res.data
-    }).then((res)=>{
+        return res.data // devuelve la respuesta de los datos del API, then se espera a que responda el resultado
+    }).then((res)=>{ // ya teniendo la respuesta de la API, junto con sus datos
         console.log(res);
         imgPoke2.src = res.sprites.front_default;
         poke2Tipo.innerHTML = res.types[0].type.name;
@@ -33,9 +56,12 @@ const obtenerPokeRival = () =>{
 
 const combate = ()=>{
     
+    const ataqueRival = parseInt(poke2Ataque.textContent);
+    const ataquePropio = parseInt(pokeAtaque.textContent);
+
 }
 
-window.addEventListener('load', obtenerPokeRival)
-
-
+window.addEventListener('load', obtenerPokeRival);
+btnElegir.addEventListener('click', obtenerPokePropio);
+btnPelear.addEventListener('click', combate);
 
